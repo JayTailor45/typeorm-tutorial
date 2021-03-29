d4d1593ba4e76516248dbd35ed7f3562c2d1a921
JAY TAILOR <tailorj64@gmail.com>
2021-03-30 00:36:42 +0530

    Perform CRUD operation

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,16 +1,11 @@
 import { Entity, Column } from "typeorm";
 import { Model } from "./Model";
 
-@Entity({name: 'users'})
+@Entity({ name: "users" })
 export class User extends Model {
+  @Column()
+  title: string;
 
   @Column()
-  name: string;
-
-  @Column()
-  email: string;
-
-  @Column()
-  role: string;
-
+  body: string;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,71 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
 import {User} from "./entity/User";
+import express, { Request, Response } from "express";
 
-createConnection().then(async connection => {
+const app = express();
+app.use(express.json());
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+app.post("/users", async (req: Request, res: Response) => {
+  const { name, email, role } = req.body;
+  try {
+    const user = User.create({ name, email, role });
+    await user.save();
+    return res.status(201).json(user);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
+app.get("/users", async (_: Request, res: Response) => {
+  try {
+    const users = await User.find();
+    return res.status(200).json(users);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
 
-    console.log("Here you can setup and run express/koa/any other framework.");
+app.put("/users/:uuid", async (req: Request, res: Response) => {
+  const uuid = req.params.uuid;
+  const { name, email, role } = req.body;
+  try {
+    const user = await User.findOneOrFail({ uuid });
 
-}).catch(error => console.log(error));
+    user.name = name || user.name;
+    user.email = email || user.email;
+    user.role = role || user.role;
+
+    await user.save();
+    return res.status(200).json(user);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+app.delete("/users/:uuid", async (req: Request, res: Response) => {
+  const uuid = req.params.uuid;
+  try {
+    const user = await User.findOneOrFail({ uuid });
+    await user.remove();
+    return res.status(204).json({ message: "User is deleted" });
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+app.get("/users/:uuid", async (req: Request, res: Response) => {
+  const uuid = req.params.uuid;
+  try {
+    const user = await User.findOneOrFail({ uuid });
+    return res.status(200).json({ user });
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+createConnection()
+  .then((async) => {
+    app.listen(5000, () => console.log(`Serer is running on port 5000`));
+  })
+  .catch((error) => console.log(error));
